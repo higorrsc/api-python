132f16bef5c4d486d4ae93c6bb77931575b55e81
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2022-12-06 23:43:21 -0300

    alteração texto remoção acentuação

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 livros = [
     {
         'id': 1,
-        'título': 'O Senhor dos Anéis - A Sociedade do Anel',
+        'titulo': 'O Senhor dos Anéis - A Sociedade do Anel',
         'autor': 'J.R.R Tolkien'
     },
     {
         'id': 2,
-        'título': 'Harry Potter e a Pedra Filosofal',
+        'titulo': 'Harry Potter e a Pedra Filosofal',
         'autor': 'J.K Howling'
     },
     {
         'id': 3,
-        'título': 'James Clear',
+        'titulo': 'James Clear',
         'autor': 'Hábitos Atômicos'
     },
 ]
